71e01e602f3f6e6d658659948e1a2b91e63df868
Joshua Burke <burke.joshua.james@gmail.com>
2018-05-18 16:14:04 -0400

    Add more tests for the preview functionality

--- cypress/integration/simple_spec.js
+++ cypress/integration/simple_spec.js
@@ -1,21 +1,35 @@
 describe('My Smoke Test', function () {
   it('Runs without fail', function () {
-    expect(true).to.equal(true)
-  })
-})
+    expect(true).to.equal(true);
+  });
+});
 
-describe('The page header', function () {
-  it('has the right words in it', function () {
-    cy.visit('localhost:5000')
-    cy.get('h1').contains('Markdown Preview')
-  })
-})
 
-describe('The Source text area', function () {
-  it('is present on the page', function () {
-    cy.visit('localhost:5000')
-    cy.get('form').within(() => {
-      cy.get('textarea#source').should('be.visible')
-    })
+describe('The Page', function () {
+
+  before(function () {
+    cy.visit('localhost:5000');
+  });
+
+  it('has a headline', function () {
+    cy.get('h1').contains('Markdown Preview');
+  });
+
+  it('has a source text area', function () {
+    cy.get('textarea#source').should('be.visible');
+  });
+
+  it('has a preview text area', function () {
+    cy.get('div#preview');
+  });
+
+  it('has a "render" button', function () {
+    cy.get('button#render');
+  });
+
+  it('converts markdown to html', function () {
+    cy.get('textarea#source').type('# Hello');
+    cy.get('button').click();
+    cy.get('div#preview').should('include', 'Hello');
   })
-})
+});
